22744e428cdf2106e77fa9c3e6c920666fefd032
Snehal <nanavaresnehal115@gmail.com>
2022-10-12 14:12:54 +0530

    ADDED[Snehal] UC3- Added Validations to User Name

--- src/main/java/com/Bridgelabz/LoginServlet.java
+++ src/main/java/com/Bridgelabz/LoginServlet.java
@@ -9,35 +9,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login servlet testing",
         urlPatterns = {"/LoginServlet"},
         initParams = {
-                @WebInitParam(name = "user", value = "Snehal"),
+                @WebInitParam(name = "user", value = "snehal"),
                 @WebInitParam(name = "password", value = "Snehal@123")
         }
 )
 public class LoginServlet extends HttpServlet {
+    private String validName = "[A-Z][a-z]{2,}";
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse respponse) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //get request parameters for username and password
-        String username = request.getParameter("user");
-        String password = request.getParameter("pwd");
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+
+
 
         //get servlet config init params
-        String userId = getServletConfig().getInitParameter("username");
-        String pass = getServletConfig().getInitParameter("password");
+        String userId = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
 
-        if (username.equals(userId) && password.equals(pass)){
-            request.setAttribute("username", username);
-            request.getRequestDispatcher("loginSuccess.jsp").forward(request,respponse);
-        }else{
-            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/Login.html");
-            PrintWriter out = respponse.getWriter();
-            out.write("<font color=red> User name or password is wrong!.</font>");
-            requestDispatcher.include(request, respponse);
-            out.close();
+        if (user.matches(validName))
+        {
+            if (user.equals(user) && userId.equals(user) && password.equals(pwd)){
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
+            }
+            else{
+                RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/Login.html");
+                PrintWriter out = response.getWriter();
+                out.write("<font color=red> User name or password is wrong!.</font>");
+                requestDispatcher.include(request, response);
+                out.close();
+            }
         }
+        else
+        {
+            PrintWriter out = response.getWriter();
+            out.write("<font color=red>Enter Valid User Name....</font>");
+        }
+
     }
 }
+
+
+
+
+
